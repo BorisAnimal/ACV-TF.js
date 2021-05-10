b7d7f6d85a707ec78e7103e4df314d9d7c15595f
Boris <iguru98@yandex.ru>
2021-05-10 15:40:38 +0300

    inference done

--- mnist-gui/public/client.js
+++ mnist-gui/public/client.js
@@ -1,36 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-    var mouse = {
+    let state = {
         click: false,
         move: false,
         pos: {x: 0, y: 0},
-        pos_prev: false
+        pos_prev: false,
+        updated: false,
+        response_received: true,
     };
     // get canvas element and create context
-    var canvas = document.getElementById('drawing');
-    var context = canvas.getContext('2d');
-    var width = canvas.width;
-    var height = canvas.height;
+    const canvas = document.getElementById('drawing');
+    const output = document.getElementById('prediction');
+    const context = canvas.getContext('2d');
+    const clearButton = document.getElementById('clear-button');
+    let width = canvas.width;
+    let height = canvas.height;
     let leftPad = canvas.getBoundingClientRect().left;
     let topPad = canvas.getBoundingClientRect().top;
-    var socket = io.connect();
+    let debug = true;
+    const socket = io.connect();
 
-    // set canvas to full browser width/height
-    canvas.width = width;
-    canvas.height = height;
+    // Button onclick events
+    clearButton.onclick = () => socket.emit('clear', {})
 
     // register mouse event handlers
-    canvas.onmousedown = function (e) {
-        mouse.click = true;
-    };
-    canvas.onmouseup = function (e) {
-        mouse.click = false;
-    };
+    canvas.onmousedown = (e) => state.click = true;
+    canvas.onmouseup = (e) => state.click = false;
+    canvas.onmouseleave = (e) => state.click = false;
 
-    canvas.onmousemove = function (e) {
+    canvas.onmousemove = (e) => {
         // normalize mouse position to range 0.0 - 1.0
-        mouse.pos.x = (e.clientX - leftPad) / width;
-        mouse.pos.y = (e.clientY - topPad) / height;
-        mouse.move = true;
+        state.pos.x = (e.clientX - leftPad) / width;
+        state.pos.y = (e.clientY - topPad) / height;
+        state.move = true;
     };
 
     // draw line received from server
@@ -42,24 +43,47 @@ document.addEventListener("DOMContentLoaded", () => {
         context.stroke();
     });
 
+    socket.on('clear', (_) => {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    })
+
+    socket.on('show_predict', (data) => {
+        console.log(data)
+        const labels = Object.values(data.labels);
+        const coords = data.coords;
+
+        console.log("pred:" + labels)
+
+        output.textContent = "" + labels;
+    });
+
     let c = 0;
 
     // main loop, running every 25ms
     function mainLoop() {
-        // c += 1;
-        // if (c % 40) {
-        //     c = 0;
-        //     let w = canvas.width;
-        //     let h = canvas.height;
-        //     socket.emit('ocr', {image: context.getImageData(0, 0, w, h)})
-        // }
+        c += 1;
+        // console.log('loop')
+
         // check if the user is drawing
-        if (mouse.click && mouse.move && mouse.pos_prev) {
+        if (state.click && state.move && state.pos_prev) {
+            console.log('draw_line')
             // send line to the server
-            socket.emit('draw_line', {line: [mouse.pos, mouse.pos_prev]});
-            mouse.move = false;
+            socket.emit('draw_line', {line: [state.pos, state.pos_prev]});
+            state.move = false;
+            state.updated = true;
         }
-        mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
+
+        // Call backend for OCR
+        if (c % 40 === 0 && state.updated && !state.click) {
+            // TODO: 1. make resize before act 2. Make async
+            state.updated = false;
+            // TODO: add position data to message
+            instanceSegmentation(canvas, debug).then((image_data) => {
+                socket.emit('ocr', {images: image_data, coords: 123});
+            })
+        }
+
+        state.pos_prev = {x: state.pos.x, y: state.pos.y};
         setTimeout(mainLoop, 25);
     }
 
---
+++ mnist-gui/public/utils.js
@@ -0,0 +1,94 @@
+function once(fn, context) {
+    var result;
+    return function () {
+        if (fn) {
+            result = fn.apply(context || this, arguments);
+            fn = null;
+        }
+        return result;
+    };
+}
+
+
+async function instanceSegmentation(canvas, debug = null) {
+    let image = IJS.Image.fromCanvas(canvas);
+    if (debug)
+        console.log("All zeros in input:", image.data.every(_ => _ === 0))
+
+    let grey = IJS.Image.createFrom(image, {
+        components: 1,
+        alpha: false,
+        colorModel: 'GREY' //GREY,
+    });
+    let ptr = 0;
+    let data = image.data;
+    for (let j = 0; j < data.length; j += image.channels) {
+        grey.data[ptr++] = data[j + 3];
+    }
+    grey = grey
+        .dilate({iterations: 3})
+
+
+    if (debug) {
+        console.log("All zeros in grey:", grey.data.every(_ => _ === 0))
+        let elem = document.getElementById("debug_grey");
+        elem.setAttribute("src", grey
+            .resize({width: 400, height: 200})
+            .toDataURL("image/png"));
+        elem.setAttribute("height", "200");
+        elem.setAttribute("width", "400");
+        elem.setAttribute("alt", "grey_img");
+        // elem.setAttribute("hidden", "false");
+        // elem.style.visibility = "visible";
+    }
+
+    let mask = grey.mask();
+    let roiManager = grey.getRoiManager();
+    roiManager.fromMask(mask);
+    let rois = roiManager.getRois({positive: true, negative: false, minSurface: 6})//.filter(_=>_.id !== -1)
+
+
+    if (debug) {
+        console.log("rois:", rois)
+        var container = document.getElementById("debug2_crops");
+        container.innerHTML = '';
+    }
+
+    return rois.map((r) => {
+        [minX, maxX, minY, maxY] = [r.minX, r.maxX, r.minY, r.maxY]
+        let width = maxX - minX
+        let height = maxY - minY
+        let side = Math.max(width, height)
+
+
+        let x = grey
+            .crop({
+                x: minX, y: minY,
+                width: width, height: height
+            })
+            .pad({
+                size: [
+                    parseInt((side - width) / 2),
+                    parseInt((side - height) / 2)
+                ],
+                color: [0],
+                algorithm: 'set'
+            })
+            .resize({
+                width: 28,
+                height: 28,
+            })
+
+        if (debug) {
+            let elem = document.createElement("img");
+            elem.setAttribute("src", x.toDataURL("image/png"));
+            elem.setAttribute("height", "28");
+            elem.setAttribute("width", "28");
+            elem.setAttribute("alt", "img");
+            container.appendChild(elem);
+        }
+
+        return Object.values(x.data)
+    })
+
+}
